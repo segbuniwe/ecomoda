f746f060225fc3ce2a4b72c564a304b4774bb456
katcontrerasdev <kat.contreras@gmail.com>
2023-10-15 11:28:10 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,jsx}", "./components/**/*.{html,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
