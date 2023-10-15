e143728abe0d2031d8af6fb8a1fe1bc729faf7b8
katcontrerasdev <kat.contreras@gmail.com>
2023-10-15 12:08:24 -0700

    updated libraries

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{html,jsx}", "./components/**/*.{html,jsx}"],
+  content: ["./screens/**/*.{html,jsx}", "./components/**/*.{html,jsx}"],
   theme: {
     extend: {},
   },
