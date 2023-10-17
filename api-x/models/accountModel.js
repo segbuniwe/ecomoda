9e937bc2c297cb6eca3c7b83662d23fe04d759d5
katcondev <kat.contreras@gmail.com>
2023-10-17 07:59:17 -0700

    added account model and route

---
+++ api-x/models/accountModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const accountSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//need to add passage auth handler/middleware
+
+const Account = mongoose.model("Account", accountSchema);
+
+export default Account;
