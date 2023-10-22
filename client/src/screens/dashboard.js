f50b6f64e4274df41bf9c1f869fc4a5ce688940b
katcondev <kat.contreras@gmail.com>
2023-10-22 14:35:24 -0700

    added layout to clothes list, create and dashboard

--- client/src/screens/dashboard.js
+++ client/src/screens/dashboard.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "../App.css";
-import UserProfile from "../components/User-Profile";
+
 import { PassageAuthGuard } from "@passageidentity/passage-react";
 import { usePassageUserInfo } from "../hooks/";
 import LogoutButton from "../components/LogoutButton";
+import Layout from "../components/Layout";
 
 const Dashboard = () => {
   const { userInfo, loading } = usePassageUserInfo();
@@ -16,27 +17,29 @@ const Dashboard = () => {
     );
   }
   return (
-    <PassageAuthGuard
-      unAuthComp={
-        <div>
-          <div>you must be logged in</div>
+    <Layout>
+      <PassageAuthGuard
+        unAuthComp={
           <div>
-            <a href='/'>Login</a>
+            <div>you must be logged in</div>
+            <div>
+              <a href='/'>Login</a>
+            </div>
           </div>
-        </div>
-      }
-    >
-      <div>
-        <div>Welcome</div>
+        }
+      >
         <div>
-          You successfully signed in with Passage. This is your homepage. <br />
-          <br />
-          Your username is: {userInfo?.email}
+          <div>Welcome</div>
+          <div>
+            You successfully signed in with Passage. This is your homepage.{" "}
+            <br />
+            <br />
+            Your username is: {userInfo?.email}
+          </div>
+          <LogoutButton />
         </div>
-        <LogoutButton />
-      </div>
-      <UserProfile />
-    </PassageAuthGuard>
+      </PassageAuthGuard>
+    </Layout>
   );
 };
 
