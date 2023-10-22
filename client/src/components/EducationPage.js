b0074b43e4297b0eaacedb980475018c633c5026
katcondev <kat.contreras@gmail.com>
2023-10-22 04:00:30 -0700

    updated ed and guidelines

--- client/src/components/EducationPage.js
+++ client/src/components/EducationPage.js
@@ -3,29 +3,31 @@ import { Link } from "react-router-dom";
 
 function EducationPage() {
   return (
-    <div className='mx-auto max-w-3xl bg-gray-50 py-6 my-12 rounded-xl'>
-      <h1>Community Tips & Videos</h1>
-      <video controls width='640' height='360'>
-        <source src={videoFile} type='video/mp4' />
-      </video>
-      <div className='shout-out'>
-        <h1>Shout Out!</h1>
-        <p>
-          Thanks to Edmond Lau and Saskia Fairfull of the IFAB (The Independent
-          Fashion Advisory Board) community for giving us even more insight into
-          sustainable and eco-friendly fashion. Please reach out to IFAB{" "}
-          <Link
-            className='link'
-            to='https://www.linkedin.com/company/independent-fashion-advisory-board/'
-          >
-            here
-          </Link>{" "}
-          to gain more knowledge and expand your horizons in sustainability.
-        </p>
+    <div className='mx-auto max-w-7xl bg-gray-50 py-6 my-12 rounded-xl'>
+      <div className='mx-auto max-w-6xl py-6 px-6'>
+        <h1 className='text-4xl font-bold py-6'>Community Tips & Videos</h1>
+        <video controls width='640' height='360'>
+          <source src={videoFile} type='video/mp4' />
+        </video>
+        <div className='shout-out'>
+          <h1>Shout Out!</h1>
+          <p>
+            Thanks to Edmond Lau and Saskia Fairfull of the IFAB (The
+            Independent Fashion Advisory Board) community for giving us even
+            more insight into sustainable and eco-friendly fashion. Please reach
+            out to IFAB{" "}
+            <Link
+              className='link'
+              to='https://www.linkedin.com/company/independent-fashion-advisory-board/'
+            >
+              here
+            </Link>{" "}
+            to gain more knowledge and expand your horizons in sustainability.
+          </p>
+        </div>
       </div>
     </div>
   );
-
 }
 
 export default EducationPage;
