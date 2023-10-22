afc4167e968f5349747bd9678c2f883d4adbb78e
katcondev <kat.contreras@gmail.com>
2023-10-21 23:55:13 -0700

    responsive menu

--- client/src/App.js
+++ client/src/App.js
@@ -11,27 +11,31 @@ import MissionPage from "./components/MissionPage";
 import TeamSocialsPage from "./components/TeamSocialsPage";
 import CommunityGuidelines from "./components/CommunityGuidelines";
 import EducationPage from "./components/EducationPage";
-import Navbar from "./components/Navbar";
 import MainPage from "./components/MainPage";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <PassageProvider appId={process.env.REACT_APP_PASSAGE_APP_ID}>
       <>
         <Router>
-          <Navbar />
+          <Menu />
           <Routes>
             <Route exact path='/' element={<Index />} />
             <Route exact path='/login' element={<Login />} />
             <Route exact path='/dashboard' element={<Dashboard />} />
-            <Route exact path="/create" element={<CreateClothingItemForm />} />
-            <Route exact path="/clothes" element={<ClothesList />} />
-            <Route exact path="/clothes/:clothes_id" element={<ClothingDetailPage />} />
-            <Route exact path="/mission" element={<MissionPage />} />
-            <Route exact path="/team" element={<TeamSocialsPage />} />
-            <Route exact path="/guidelines" element={<CommunityGuidelines />} />
-            <Route exact path="/educate" element={<EducationPage />} />
-            <Route exact path="/home" element={<MainPage />} />
+            <Route exact path='/create' element={<CreateClothingItemForm />} />
+            <Route exact path='/clothes' element={<ClothesList />} />
+            <Route
+              exact
+              path='/clothes/:clothes_id'
+              element={<ClothingDetailPage />}
+            />
+            <Route exact path='/mission' element={<MissionPage />} />
+            <Route exact path='/team' element={<TeamSocialsPage />} />
+            <Route exact path='/guidelines' element={<CommunityGuidelines />} />
+            <Route exact path='/educate' element={<EducationPage />} />
+            <Route exact path='/home' element={<MainPage />} />
           </Routes>
         </Router>
       </>
