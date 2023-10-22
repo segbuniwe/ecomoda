98d80abceb9cfe87bbd22be8f89875df987f5f6c
katcondev <kat.contreras@gmail.com>
2023-10-22 09:04:05 -0700

    updated login

--- client/src/components/Login-Form.js
+++ client/src/components/Login-Form.js
@@ -3,12 +3,10 @@ import { PassageAuth } from "@passageidentity/passage-react";
 
 const LoginForm = () => {
   return (
-    <div className='mx-auto max-w-3xl bg-gray-100 py-6 my-12 rounded-xl h-[60vh]'>
-      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
-        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-          {" "}
-          <PassageAuth />
-        </div>
+    <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+        {" "}
+        <PassageAuth />
       </div>
     </div>
   );
