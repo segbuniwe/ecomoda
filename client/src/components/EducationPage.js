6673332e5e657d03dc8848c0a3ff40fb1775c078
katcondev <kat.contreras@gmail.com>
2023-10-22 20:57:01 -0700

    updated styles

--- client/src/components/EducationPage.js
+++ client/src/components/EducationPage.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 function EducationPage() {
   return (
-    <div className='mx-auto max-w-7xl bg-gray-100 py-6 my-12 rounded-xl'>
-      <div className='mx-auto max-w-6xl py-6 px-6'>
-        <h1 className='text-4xl font-bold py-6'>Community Tips & Videos</h1>
-        <video controls width='640' height='360'>
+    <div className='mx-auto max-w-6xl bg-gray-100 py-6 my-12 rounded-xl'>
+      <div className='mx-auto max-w-5xl py-6 px-6'>
+        <h1 className='text-4xl py-6'>Community Tips & Videos</h1>
+        <video controls width='1280' height='720'>
           <source src={videoFile} type='video/mp4' />
         </video>
-        <div className='shout-out'>
-          <h1>Shout Out!</h1>
+        <div className='py-6'>
+          <h1 className='text-xl py-3'>Shout Out!</h1>
           <p>
             Thanks to Edmond Lau and Saskia Fairfull of the IFAB (The
             Independent Fashion Advisory Board) community for giving us even
