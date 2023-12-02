4237780962825937b2a9a22a8da6a59e3c259209
katcondev <kat.contreras@gmail.com>
2023-12-02 11:59:12 -0800

    missed updates

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -38,20 +38,20 @@ const navigation = [
 
 export default function Footer() {
   return (
-    <footer className='bg-emo-teal'>
+    <footer className='bg-emo-teal fixed bottom-0 left-0 w-full'>
       <div className='mx-auto max-w-7xl px-6 py-3 md:flex md:items-center md:justify-between lg:px-8'>
-        <div className='flex justify-center space-x-6 md:order-2'>
-          {navigation.map((item) => (
-            <a key={item.name} href={item.href} className=' hover:text-emo-tan'>
-              <span className='sr-only'>{item.name}</span>
-              <item.icon className='h-6 w-6' aria-hidden='true' />
-            </a>
-          ))}
-        </div>
         <div className='mt-8 md:order-1 md:mt-0'>
           <p className='text-center text-sm leading-5'>
             &copy; 2023 EcoModa, Inc. All rights reserved.
           </p>
+        </div>
+        <div className='flex justify-center space-x-6'>
+          {navigation.map((item) => (
+            <a key={item.name} href={item.href} className='hover:text-emo-tan'>
+              <span className='sr-only'>{item.name}</span>
+              <item.icon className='h-6 w-6' aria-hidden='true' />
+            </a>
+          ))}
         </div>
       </div>
     </footer>
